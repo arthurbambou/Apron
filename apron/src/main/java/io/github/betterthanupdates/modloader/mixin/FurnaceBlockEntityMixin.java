6f32f0b7a57de50e7a654ad3aaa353314f276f81
Cat Core <arthur.koalas@free.fr>
2024-11-02 16:45:47 +0100

    Fix crash with BNB by getting rid of an overwrite

--- apron/src/main/java/io/github/betterthanupdates/modloader/mixin/FurnaceBlockEntityMixin.java
+++ apron/src/main/java/io/github/betterthanupdates/modloader/mixin/FurnaceBlockEntityMixin.java
@@ -1,8 +1,10 @@
 package io.github.betterthanupdates.modloader.mixin;
 
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
 import modloader.ModLoader;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,84 +21,16 @@ import net.minecraft.item.ItemStack;
 @Mixin(FurnaceBlockEntity.class)
 public abstract class FurnaceBlockEntityMixin extends BlockEntity implements Inventory {
 	@Shadow
-	public int burnTime;
-
-	@Shadow
-	protected abstract boolean canAcceptRecipeOutput();
-
-	@Shadow
-	public int fuelTime;
-
-	@Shadow
-	protected abstract int getFuelTime(ItemStack stack);
-
-	@Shadow
 	private ItemStack[] inventory;
 
-	@Shadow
-	public abstract boolean isBurning();
-
-	@Shadow
-	public int cookTime;
-
-	@Shadow
-	public abstract void craftRecipe();
-
-	/**
-	 * @author Risugami
-	 * @reason idk
-	 * TODO(halotroop2288): rewrite as an {@link Inject} Mixin
-	 */
-	@Environment(EnvType.CLIENT)
-	@Overwrite
-	public void tick() {
-		boolean flag = this.burnTime > 0;
-		boolean flag1 = false;
-
-		if (this.burnTime > 0) {
-			--this.burnTime;
+	@WrapWithCondition(method = "tick", at = @At(value = "FIELD", target = "Lnet/minecraft/item/ItemStack;count:I", opcode = Opcodes.PUTFIELD))
+	private boolean modloader$tick(ItemStack instance, int value) {
+		if (this.inventory[1].getItem().hasContainerItemType()) {
+			this.inventory[1] = new ItemStack(this.inventory[1].getItem().getContainerItemType());
+			return false;
 		}
 
-		if (!this.world.isClient) {
-			if (this.burnTime == 0 && this.canAcceptRecipeOutput()) {
-				this.fuelTime = this.burnTime = this.getFuelTime(this.inventory[1]);
-
-				if (this.burnTime > 0) {
-					flag1 = true;
-
-					if (this.inventory[1] != null) {
-						if (this.inventory[1].getItem().hasContainerItemType()) {
-							this.inventory[1] = new ItemStack(this.inventory[1].getItem().getContainerItemType());
-						} else {
-							--this.inventory[1].count;
-						}
-
-						if (this.inventory[1].count == 0) {
-							this.inventory[1] = null;
-						}
-					}
-				}
-			}
-
-			if (this.isBurning() && this.canAcceptRecipeOutput()) {
-				if (++this.cookTime == 200) {
-					this.cookTime = 0;
-					this.craftRecipe();
-					flag1 = true;
-				}
-			} else {
-				this.cookTime = 0;
-			}
-
-			if (flag != this.burnTime > 0) {
-				flag1 = true;
-				FurnaceBlock.updateFurnaceState(this.burnTime > 0, this.world, this.x, this.y, this.z);
-			}
-		}
-
-		if (flag1) {
-			this.markDirty();
-		}
+		return true;
 	}
 
 	@Inject(method = "getFuelTime", cancellable = true, at = @At("RETURN"))
